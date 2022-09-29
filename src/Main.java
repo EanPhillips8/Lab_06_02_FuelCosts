7089ff83791f66caf72eac848a05506e04417cb5
EanPhillips8 <eanphillips8@gmail.com>
2022-09-29 15:20:41 -0400

    'kipiuguk'

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+
+        double tank = 0;
+        double mpg = 0;
+        double price = 0;
+        String Trash = "";
+
+
+        System.out.print("\n How many gallons of gas can you store in your tank: ");
+        if(in.hasNextDouble()) {
+            tank = in.nextDouble();
+        }
+        else {
+            Trash = in.nextLine();
+            System.out.println("Error, please enter a valid number");
+        }
+
+        System.out.print("\n How many miles per gallon does your car get: ");
+        if(in.hasNextDouble()) {
+            mpg = in.nextDouble();
+        }
+        else {
+            Trash = in.nextLine();
+            System.out.println("Error, please enter a valid number");
+        }
+
+        System.out.print("\n What is the price of gas: ");
+        if(in.hasNextDouble()) {
+            price = in.nextDouble();
+        }
+        else {
+            Trash = in.nextLine();
+            System.out.println("Error, please enter a valid number");
+        }
+        System.out.println("The cost for you to drive 100 miles is: " + ((100/mpg) * price)  + " dollars");
+
+        System.out.println("You can drive " + (tank * mpg) + " with a full tank");
     }
 }
